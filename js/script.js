f78c843f615dd8becb3092302915a7ab39218975
mstmehotabin0329-ops <mstmehotabin0329@gmail.com>
2026-03-30 22:19:08 +0600

    Start JavaScript functionality

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let activeTab = "all"
+
+
+let totalCount = document.getElementById('total');
+let interviewCount = document.getElementById('interview');
+let rejectedCount = document.getElementById('rejected');
+let availableCount = document.getElementById("availableCount");
+
+
+const allContainer = document.getElementById("all-container");
+const interviewContainer = document.getElementById("interview-container");
+const rejectedContainer = document.getElementById("rejected-conainer");
+const emptyState = document.getElementById('no-data');
+
+
+
